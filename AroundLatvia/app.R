687ab8aabc96be5fa43eb7868bca7a0f0f290b55
Martin Møller Skarbiniks Pedersen <traxplayer@gmail.com>
2021-03-01 00:21:27 +0100

    Update app.R

--- AroundLatvia/app.R
+++ AroundLatvia/app.R
@@ -5,7 +5,7 @@ library(wesanderson) #wes.palette
 
 ui <- fluidPage(
     titlePanel("Around Latvia"),
-    a("Source code:", href = "https://github.com/MartinMSPedersen/GenerativeR/tree/master/aroundlatvia"),
+    a("Source code:", href = "https://github.com/MartinMSPedersen/GenerativeR/tree/master/AroundLatvia"),
     wellPanel(
         sliderInput("seed",
                     "Seed value:",
