62e7073fc307d8ff5a3b542a1083885637f89930
Martin M. S. Pedersen <traxplayer@gmail.com>
2025-04-18 00:42:30 +0200

    x

--- VelvetQuasar/server.R
+++ VelvetQuasar/server.R
@@ -2,20 +2,28 @@ library(shiny)
 library(jsonlite) # base64_enc
 library(svglite)
 library(broman) # hex2dec
-library(ggplot2)
+library(ragg)     # For modern graphics devices
+library(showtext) # For loading custom fonts
 
+#font_add("NotoSans", regular = "fonts/NotoSans-Regular.ttf")
+showtext_auto()
+options(shiny.useragg = TRUE)
 
 shinyServer(function(input, output, session) {
     observe({
         input$randomvalues
         updateSliderInput(session, "seed", value = sample(1:1e5, 1))
-        updateSliderInput(session, "layout", value = sample(0:6, 1))
+        updateSliderInput(session, "layout", value = sample(0:5, 1))
     })
     
     plotInput <- function() {
+      plot.new() 
+      par(mar = c(0,0,0,0)) # No margins
+      plot.window(xlim = c(1,size), ylim = c(1,size), asp = 1) # Square aspect ratio
+      
       set.seed(input$seed)
       size <- 1000
-      plot.window(c(1,size), c(1,size))
+      plot.window(xlim = c(1,size), ylim = c(1,size))
       z <- 40
       always <- TRUE
       noise_level <- 10
@@ -27,19 +35,16 @@ shinyServer(function(input, output, session) {
         chars <- as.numeric("0x13254"):as.numeric("0x13256") # 𓉔 𓉕  𓉖
       }
       if (input$layout == 2) {
-        chars <- as.numeric("0x13255"):as.numeric("0x13256")
-      } 
-      if (input$layout == 3) {
         chars <- as.numeric(c("0x1F796", "0x1F78B"))
       }
-      if (input$layout == 4) {
+      if (input$layout == 3) {
         chars <- hex2dec("1F260"):hex2dec("1F265") # Luck, Prosperity, Longevity, Happiness, Wealth, Double-Happiness
         noise_level <- 0
       }
-      if (input$layout == 5) {
+      if (input$layout == 4) {
         chars <- as.numeric(c("0x27D5", "0x27D6", "0x27D7")) # ⟕ ⟖ ⟗ 
       }        
-      if (input$layout == 6) {
+      if (input$layout == 5) {
         chars <- as.numeric(97:121)
         z <- z*0.75
       }
@@ -57,7 +62,7 @@ shinyServer(function(input, output, session) {
           }
         }
       }
-      box()
+      #box()
     }
     
     output$quasar <- renderPlot({
@@ -66,33 +71,38 @@ shinyServer(function(input, output, session) {
 
     output$png_download <- downloadHandler(
       filename = function() {
-        paste0("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".png")
+        paste("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".png", sep = "")
       },
       content = function(file) {
-        png(file, width = 800, height = 800, units = "px", res = 100) 
-        plotInput() 
+        agg_png(file, width = 800, height = 800, units = "px", res = 96)
+        plotInput()
+        box()
         dev.off()
       }
     )
 
     output$pdf_download <- downloadHandler(
       filename = function() {
-        paste0("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".pdf")
+        paste("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".pdf", sep = "")
       },
       content = function(file) {
-        pdf(file, title = "Velvet Quasar", paper = "A4", width = 8, height = 8)
-        plotInput() # Call the plot function to draw on the pdf device
-        dev.off() # Close the device
-      }
+        pdf(file, title = "Velvet Quasar", family = "Helvetica", paper = "A4", width = 8, height = 8)
+        plotInput()
+        dev.off()
+      },
+      contentType = "application/pdf"
     )
     
     output$svg_download <- downloadHandler(
       filename = function() {
-        paste0("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".svg")
+        paste("velvetquasar-", base64_enc(paste(input$seed, input$layout, sep = ":")), ".svg", sep = "")
       },
       content = function(file) {
         svglite(file, width = 8, height = 8)
+        plot.new()
+        par(mai = c(0, 0, 0, 0))
         plotInput()
+        box()
         dev.off()
       }
     )
--- VelvetQuasar/ui.R
+++ VelvetQuasar/ui.R
@@ -1,31 +1,20 @@
 library(shiny)
 
 shinyUI(fluidPage(
-    titlePanel("Velvet Quasar"),
-    a("Source code:", href = "https://github.com/MartinMSPedersen/GenerativeR/tree/master/VelvetQuasar"),
-    wellPanel(
-        fluidRow(
-            column(
-                width = 10,
-                sliderInput("seed", "Seed value:",min = 1, max = 1e5, value = sample(1:50000,1))
-            ),
-            column(
-                width = 2,
-                radioButtons("layout", "Character Set:", 0:6, selected = 5)
-            )
-        )
-    ),
-    fluidRow(
-        column(12,
-               actionButton("randomvalues","Set random values"),
-               downloadButton("svg_download", label = "Download as svg"),
-               downloadButton("png_download", label = "Download as png"),
-               downloadButton("pdf_download", label = "Download as pdf")
-        )
-    ),
-    fluidRow(
-      column(width = 12,
-             plotOutput("quasar", width = "800px", height = "800px")
+    titlePanel("Velvet Quasar Generator"),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("seed", "Seed", min = 1, max = 1e5, value = sample(1:1e5, 1), step = 1),
+        sliderInput("layout", "Character Set", min = 0, max = 5, value = sample(0:5, 1), step = 1),
+        actionButton("randomvalues", "Randomize Parameters"),
+        hr(),
+        downloadButton("png_download", "Download PNG"),
+        downloadButton("pdf_download", "Download PDF"),
+        downloadButton("svg_download", "Download SVG")
+      ),
+      mainPanel(
+        plotOutput("quasar") 
       )
     )
-))
+  )
+)
